276bc8cbe3b7b3e8022f44b758c3cfec2c38e9ac
Austin Kelsay <53542748+AustinKelsay@users.noreply.github.com>
2020-07-22 11:49:30 -0500

    Setting up file struc, index, and server

---
+++ index.js
@@ -0,0 +1,12 @@
+const server = require('./server.js');
+const express = require('express');
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+});
+
+server.get('/', (req, res) => {
+  res.status(200).json({message: "Welcome!"})
+})
+  
